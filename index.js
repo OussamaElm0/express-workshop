1b61f99c2358cfecc8490615b5514f7c38851e0d
OussamaElm0 <oussama.elmabrouki26@gmail.com>
2025-03-28 15:24:52 +0000

    Get all users

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ const User = require("./User")
 const app = express()
 const PORT = 8080
 
+app.get("/users", async (req, res) => {
+    try {
+        const users = await User.find()
+        res.json(users)
+    } catch (e) {
+        res.json({error: e})
+    }
+})
+
 try {
     // Start the server and listen on the specified port
     app.listen(PORT, () => console.log(`Server is running on ${PORT}`))
@@ -12,6 +21,7 @@ try {
     //Connect to database
     mongoose.connect("mongodb://localhost:27017/ngos")
     console.log("Connected to database successfully!");
+
 } catch (e){
     console.error(e);
 }
