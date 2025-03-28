934222c24307f4048f5c86d7df30c632bdec119a
OussamaElm0 <oussama.elmabrouki26@gmail.com>
2025-03-28 14:52:50 +0000

    Connect to database

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require("express")
+const mongoose = require("mongoose")
 
 const app = express()
 const PORT = 8080
 
 try {
+    // Start the server and listen on the specified port
     app.listen(PORT, () => console.log(`Server is running on ${PORT}`))
+
+    //Connect to database
+    mongoose.connect("mongodb://localhost:27017/ngos")
+    console.log("Connected to database successfully!");
 } catch (e){
     console.error(e);
 }
